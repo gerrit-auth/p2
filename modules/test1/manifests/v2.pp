92d12bb095f5e2df82a7bf96b7b53a6f42f075b6
gerrit_auth <Ruslan.Lutsenko@spilgames.com>
2013-08-27 16:07:09 +0200

    T1 second try
    Change-Id: I6c51fffcfcac0d35c61882be39140fa78ba3b6e3

--- modules/test1/manifests/v2.pp
+++ modules/test1/manifests/v2.pp
@@ -1,6 +1,6 @@
 # Install and configure an NTP server
 # You should feel free to expand on this and document any parameter
 class test{
-  notice{'Hallo  World':}
+  notice{'Hallo  Worldo':}
   notice{'Hallo  World2':}
 }
